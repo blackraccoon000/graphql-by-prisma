94d5c28bdb0b63d662593502692ba223c91ab45f
Yutaka Fujii <yutakaf@playwell.site>
2022-11-01 00:37:04 +0900

    PrismaClientの練習

--- src/context/index.ts
+++ src/context/index.ts
@@ -5,18 +5,161 @@ const prisma = new PrismaClient()
 
 export const context: Context = { prisma }
 
-// prisma.user.findMany().then((res) => {
-//   console.log('test1:', res)
-//   prisma.user
-//     .create({
+// prisma.user
+//   .findMany()
+//   .then((res) => {
+//     console.log('test1:', res)
+//     return prisma.user.create({
 //       data: {
-//         name: 'xxx_test3',
-//         email: 'xxx_test3@example.com',
+//         name: 'xxx_test4',
+//         email: 'xxx_test4@example.com',
 //       },
 //     })
-//     .then((res) => {
-//       console.log('created:', res)
-//       prisma.user.findMany().then((res) => console.log('test2:', res))
+//   })
+//   .then((res) => {
+//     console.log('created:', res)
+//     return prisma.user.findMany()
+//   })
+//   .then((res) => console.log('test2:', res))
+//   .catch((error) => console.log('error:', error))
+
+// prisma.post
+//   .findMany()
+//   .then((res) => {
+//     console.log({ res })
+//     console.log('==========')
+//     return prisma.post
+//       .create({
+//         data: {
+//           title: 'GraphQL test Title',
+//           content: 'all identity update error object',
+//           published: true,
+//           author: {
+//             connect: {
+//               email: 'xxx_text3@example.com',
+//             },
+//           },
+//         },
+//       })
+//       .then((res) => {
+//         console.log({ res })
+//         console.log('==========')
+//         // return prisma.post.findMany()
+//       })
+//   })
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+
+// prisma.user
+//   .findUnique({
+//     where: {
+//       email: 'xxx_test@example.com',
+//     },
+//   })
+//   .then((res) => {
+//     return prisma.post.create({
+//       data: {
+//         title: 'GraphQL test Title',
+//         content: 'all identity update error object',
+//         published: true,
+//         author: {
+//           connect: { email: 'xxx_test@example.com' },
+//         },
+//       },
 //     })
-//     .catch((error) => console.log('error:', error))
-// })
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// prisma.post
+//   .create({
+//     data: {
+//       title: 'GraphQL test Title',
+//       content: 'all identity update error object',
+//       published: true,
+//       author: {
+//         connect: { email: 'xxx_test@example.com' },
+//       },
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// prisma.post
+//   .create({
+//     data: {
+//       title: 'GraphQL test Title',
+//       content: 'all identity update error object',
+//       published: true,
+//       author: {
+//         connect: { email: 'xxx_text@example.com' },
+//       },
+//       // author: {
+//       //   connect: {
+//       //     email: 'xxx_text@example.com',
+//       //   },
+//       // },
+//     },
+//   })
+//   .then((res) => {
+//     console.log({ res })
+//   })
+
+// prisma.user
+//   .findUnique({
+//     where: {
+//       email: 'xxx_test@example.com',
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// prisma.post
+//   .findMany()
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// prisma.post
+//   .update({
+//     data: {
+//       published: false,
+//     },
+//     where: {
+//       id: 4,
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// prisma.post
+//   .delete({
+//     where: {
+//       id: 3,
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err.meta.cause)
+//   })
